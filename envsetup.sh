4f79f6d65d5908b76939d6ad1713e581904bd250
Christian Köstlin <christian.koestlin@gmail.com>
2012-03-25 14:24:03 +0200

    automatically iterator over *-activator.sh files

--- envsetup.sh
+++ envsetup.sh
@@ -1,5 +1,16 @@
-SCRIPT_PATH=$(dirname $BASH_SOURCE)
-source $SCRIPT_PATH/java.sh
-source $SCRIPT_PATH/android.sh
-source $SCRIPT_PATH/rvm.sh
+SCRIPT_PATH=$(dirname ${BASH_SOURCE:-$0})
 
+#echo $OSTYPE -> linux-gnu or Darwin
+#if [ -n "$ZSH_VERSION" ]
+#then
+#  echo "zsh in use"
+#else
+#  echo "no zsh in use"
+#fi
+
+
+for f in $SCRIPT_PATH/*-activator.sh
+do
+  echo "Processing $f"
+  source $f
+done
