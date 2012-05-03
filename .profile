016c5190177157aa494e89f510f6627c203e74a7
Christian Köstlin <christian.koestlin@gmail.com>
2012-05-03 10:13:19 +0200

    added linux-desktop files, .profile, .zshrc

---
+++ .profile
@@ -0,0 +1,7 @@
+echo ".profile"
+source ~/Dropbox/Documents/_projects/dot-files/envsetup.sh
+
+export LANGUAGE="en_US:en"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
--- Rakefile.rb
+++ Rakefile.rb
@@ -1,25 +1,34 @@
 D = File.dirname(__FILE__)
 H = ENV['HOME']
 
-KeyboardSettings = "#{H}/.gconf/desktop/gnome/peripherals/keyboard/kbd"
+apps = ['ScreenlayoutBeam', 'ScreenlayoutWork', 'eclipse_adt', 'eclipse_cdt']
+apps.each do |app|
+  file_name = File.join(D, '.local', 'share', 'applications', "#{app}.desktop")
+  desc "link #{file_name}"
+  task app do
+    cd H do
+      sh "ln -svf #{file_name}"
+    end
+  end
+end
 
+KeyboardSettings = "#{H}/.gconf/desktop/gnome/peripherals/keyboard/kbd"
 directory KeyboardSettings
 task :gconf => KeyboardSettings do
   sh "ln -sfv #{D}/.gconf/desktop/gnome/peripherals/keyboard/kbd/%gconf.xml #{H}/.gconf/desktop/gnome/peripherals/keyboard/kbd/%gconf.xml"
 end
 
-task :inputrc do
-  cd ENV['HOME'] do
-    sh "ln -sfv #{D}/.inputrc"
+
+dot_files = ['.inputrc', '.gitconfig', '.profile', '.joerc', '.zshrc', '.screenlayout']
+dot_files.each do |f|
+  file_name = File.join(D, f)
+  desc "link #{file_name}"
+  task f do
+    cd H do
+      sh "ln -sfv #{file_name}"
+    end
   end
 end
-
-task :gitconfig do
-  cd ENV['HOME'] do
-    sh "ln -sfv #{D}/.gitconfig"
-  end
-end
-
 
 desc 'links the needed files to $HOME'
-task :install => [:inputrc, :gitconfig, :gconf]
+task :install => dot_files + apps + [:gconf]
