958de4dd01d41eb48c6639e416da55d87402865d
Christian Köstlin <christian.koestlin@gmail.com>
2025-08-02 06:35:44 +0200

    Add my ergodox config
    Change-Id: Ia4d58cd3c0c63a2bd61d4d90a3328f160ba751fa

--- setkeyboard.rb
+++ setkeyboard.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 `xinput`.force_encoding('UTF-8').each_line.map do |l|
-  l.match(/HID 05f3:0007.*?id=(\d+?)\s.*/)
+  l.match(/HID 05f3:0007.*?id=(\d+?)\s.*/) ||
 #    l.match(/Apple Inc. Apple Keyboard.*?id=(\d+?)\s.*/) ||
-#    l.match(/.*Ergodox.*id=(\d+?)\s.*/i)
+    l.match(/.*Ergodox.*id=(\d+?)\s.*/i)
 end.compact.each do |i|
   id = i[1]
   command = "xkbcomp -i #{id} -I$HOME/.xkb ~/.xkb/keymap/kinesis $DISPLAY 2> /dev/null"
