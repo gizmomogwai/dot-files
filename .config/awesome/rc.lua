bedc86daa5b1d82fdb5274b0c628b86428eeaea8
Christian Köstlin <christian.koestlin@gmail.com>
2025-08-07 21:52:01 +0200

    Remove some unused shortcuts that get in the way if the cat jumps on the keyboard
    Change-Id: I9cb973c58ec55a7ae55785b5748b902ee245405a

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -395,16 +395,16 @@ clientkeys = gears.table.join(
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
-              {description = "close", group = "client"}),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
-              {description = "toggle floating", group = "client"}),
+    -- awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
+    --          {description = "close", group = "client"}),
+    -- awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
+    --          {description = "toggle floating", group = "client"}),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
               {description = "move to master", group = "client"}),
     awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
               {description = "move to screen", group = "client"}),
-    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
-              {description = "toggle keep on top", group = "client"}),
+    -- awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+    --          {description = "toggle keep on top", group = "client"}),
     -- awful.key({ modkey,           }, "n",
     --     function (c)
     --         -- The client currently has the input focus, so it cannot be
@@ -418,18 +418,18 @@ clientkeys = gears.table.join(
             c:raise()
         end ,
         {description = "(un)maximize", group = "client"}),
-    awful.key({ modkey, "Control" }, "m",
-        function (c)
-            c.maximized_vertical = not c.maximized_vertical
-            c:raise()
-        end ,
-        {description = "(un)maximize vertically", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "m",
-        function (c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c:raise()
-        end ,
-        {description = "(un)maximize horizontally", group = "client"}),
+    -- awful.key({ modkey, "Control" }, "m",
+    --     function (c)
+    --         c.maximized_vertical = not c.maximized_vertical
+    --         c:raise()
+    --     end ,
+    --     {description = "(un)maximize vertically", group = "client"}),
+    -- awful.key({ modkey, "Shift"   }, "m",
+    --     function (c)
+    --         c.maximized_horizontal = not c.maximized_horizontal
+    --         c:raise()
+    --     end ,
+    --     {description = "(un)maximize horizontally", group = "client"}),
     awful.key({modkey}, "u",
     function (c)
         c.left = 0
@@ -637,7 +637,7 @@ os.execute("xautolock -time 4 -locker slock &")
 -- os.execute("bash -c $HOME/.screenlayout/work.sh &")
 os.execute("ruby $HOME/Sync/dot-files/setkeyboard.rb &")
 os.execute("$HOME/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox &")
-os.execute("xset r rate 220 70")
+-- os.execute("xset r rate 220 70")
 os.execute("copyq &")
 os.execute("$HOME/run_jenkins.sh &")
 
