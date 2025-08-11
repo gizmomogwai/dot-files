ff9a9972dc136e2da0e68f0b538b277ff8571a67
Christian Köstlin <christian.koestlin@gmail.com>
2025-08-11 20:59:53 +0200

    Fix sound widget
    Change-Id: I030697a37365c54ee4f5114f07a5b7d92e93b907

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -253,7 +253,7 @@ awful.screen.connect_for_each_screen(function(s)
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
-            volume_widget{widget_type = 'horizontal_bar'},
+            volume_widget{widget_type = 'horizontal_bar', device = 'default' },
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
             wibox.widget.systray(),
